906b1a6949cbff85f19365ceb9813cd72d0a1729
Grace Decatur <gdecatur@wustl.edu>
2023-02-06 20:50:58 +0100

    iceblock spawning

--- Assets/Scripts/IceblockSpawner.cs
+++ Assets/Scripts/IceblockSpawner.cs
@@ -2,36 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SnowmanSpawn : MonoBehaviour
+public class IceblockSpawner : MonoBehaviour
 {
-    public GameObject ObsSnowman;
+    public GameObject iceblock;
     private float timer = 0;
     private float spawnTimer;
-    private float timeMin = 3;
-    private float timeMax = 7;
+    private float timeMin = 5;
+    private float timeMax = 9;
     private float distanceMin = 2;
     private float distanceMax = 5;
-    
+
+    // Start is called before the first frame update
     void Start()
     {
+        
     }
 
+    // Update is called once per frame
     void Update()
     {
         timer -= Time.deltaTime;
         if (timer <= 0)
         {
-            //Debug.Log("HERE!");
-            SpawnNewObstacle();
+            SpawnNewReward();
             spawnTimer = Random.Range(timeMin, timeMax);
             timer = spawnTimer;
         }
     }
 
-    private void SpawnNewObstacle()
+    private void SpawnNewReward()
     {
         Vector3 spawnPosition = transform.position + Vector3.right * Random.Range(distanceMin, distanceMax);
         spawnPosition.z = 0;
-        Instantiate(ObsSnowman, spawnPosition, Quaternion.identity);
+        Instantiate(iceblock, spawnPosition, Quaternion.identity);
     }
 }
